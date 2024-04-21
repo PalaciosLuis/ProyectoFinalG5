a838bf2aba0fb71b46ebb4b4a6c1b5914313522a
Luis Javier <924333523javier@gmail.com>
2024-04-21 12:22:08 -0500

    Fisrt commit group 5

---
+++ src/test/java/pe/edu/cibertec/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
